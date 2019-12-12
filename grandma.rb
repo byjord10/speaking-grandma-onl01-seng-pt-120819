adb5ddb7b44fff662a4f1e10dfe472bbefed9f0a
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-12 15:55:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,9 +7,9 @@ def speak_to_grandma(phrase)
     puts 'HUH?! SPEAK UP, SONNY!'
   else
     return "NO, NOT SINCE 1938!"
-  end 
+  end
 end 
-    
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
