fc0943685a0c1049f2cba50f845b8c21223e39b0
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-12 15:56:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == 'IF LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+  if phrase == "IF LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
   if phrase != phrase.uppercase()
-    return 'HUH?! SPEAK UP, SONNY!'
+    return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
   end
