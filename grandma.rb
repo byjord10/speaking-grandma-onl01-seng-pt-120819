74bc77e346885c798d0fad22b6cee1e367cfae5c
Brianna Jordan <bryvjordan9@gmail.com>
2019-12-12 15:58:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase != phrase.uppercase()
+  if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
